ad24bed109c148ec2298000a6617d101dd649478
Vicente <70909800+vicentefelipechile@users.noreply.github.com>
2025-08-20 12:35:04 -0400

    feat: add main entry point to package.json and create index.ts for module exports

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { ModularCommand, ModularButton, RegisterCommand } from "./modularcommand";
+import ModularModal from "./modularmodal";
+import { LOCALE_DELAY, LOCALE_ERROR, LOCALE_FORBIDDEN, LOCALE_NSFW, FormatSecondsLocale } from "./locales";
+
+export default ModularCommand;
+export { ModularCommand };
+export { ModularButton };
+export { ModularModal };
+export { RegisterCommand };
+export { FormatSecondsLocale };
+export { LOCALE_DELAY };
+export { LOCALE_ERROR };
+export { LOCALE_FORBIDDEN };
+export { LOCALE_NSFW };
